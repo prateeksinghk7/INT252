3de95a8a7bcef22e4d6d6dda3aee21d992341076
Prateek Singh <prateeksinghk7@gmail.com>
2023-04-13 23:59:29 +0530

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,30 @@
 import {Component} from "react";
-// import "./NavbarStyles.css";
-import{Link} from "react-router-dom";
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+// import{Link} from "react-router-dom";
+
 class Navbar extends Component{
 render() {
     return(
         <nav className="NavbarItems">
 
             <h1 className="navbar-logo">Trip</h1>
-            <ul className="nav-menu"><li>
-                <li>
-                    <a href="/">
-                    <i className="fa-solid fa-house-user"></i>Home
-                    
+            <ul className="nav-menu">
+              {MenuItems.map((item,index)=>{
+
+                return(
+                <li key={index}>
+                    <a className={item.cName} href="/">
+                    <i className={item.icon}></i>
+                    {item.title}
                     </a>
                 </li>
-                </li>
+                )
+              })}
+                <button>Sign Up</button>
                 </ul>
-        </nav>
-    )
+                </nav>
+    );
 }
 }
 
